608570371ee64507dfe906d0401f2b63b56d1d35
milesdavis71 <hevesiistvan71@hotmail.com>
2023-06-28 11:04:59 +0200

    5-8. Navigation Menus (Continued) kész

--- app/public/wp-content/themes/unlock-wp-theme/header.php
+++ app/public/wp-content/themes/unlock-wp-theme/header.php
@@ -16,7 +16,10 @@
             <div class="site-header__menu group">
                 <nav class="main-navigation">
                     <ul class="min-list group">
-                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                        <li
+                            <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>>
+                            <a href="<?php echo site_url('/about-us') ?>">About Us</a>
+                        </li>
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
