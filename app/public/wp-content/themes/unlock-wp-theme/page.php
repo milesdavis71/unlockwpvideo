9f62d5344e83522c9c0196d51da774c767571dab
milesdavis71 <hevesiistvan71@hotmail.com>
2023-06-28 10:13:20 +0200

    5 Menu of Child Page Link kész

--- app/public/wp-content/themes/unlock-wp-theme/page.php
+++ app/public/wp-content/themes/unlock-wp-theme/page.php
@@ -1,8 +1,8 @@
 <?php
 
-  get_header();
+get_header();
 
-  while(have_posts()) {
+while (have_posts()) {
     the_post(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image"
@@ -17,21 +17,37 @@
 <div class="container container--narrow page-section">
 
     <?php
-    $theParent = wp_get_post_parent_id(get_the_ID());
-    if ($theParent){ ?>
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) { ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home"
                     aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span
                 class="metabox__main"> <?php the_title() ?></span></p>
     </div>
     <?php
-    }?>
+        } ?>
 
+    <?php
+        $testArray = get_pages(array(
+            'child_of' => get_the_ID()
+        ));
+
+        if ($theParent or  $testArray) { ?>
     <div class="page-links">
-        <h2 class="page-links__title">About Us</h2>
+        <h2 class="page-links__title"><a
+                href="<?php echo get_the_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
         <ul class="min-list">
+            <?php
+                    ($theParent) ? $findChildrenOf = $theParent : $findChildrenOf = get_the_ID();
+                    wp_list_pages(array(
+                        'title_li' => null,
+                        'child_of' => $findChildrenOf,
+                        'sort_column' => 'menu_order'
+                    ))
+                    ?>
         </ul>
     </div>
+    <?php } ?>
 
     <div class="generic-content">
         <?php the_content() ?>
@@ -42,6 +58,6 @@
 
 <?php }
 
-  get_footer();
+get_footer();
 
 ?>
