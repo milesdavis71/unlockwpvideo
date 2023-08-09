0e8d646e962981949c4033f1faec4be27ac88c6f
unknown <hevesiistvan71@hotmail.com>
2023-08-09 18:54:40 +0200

    037 Displaying Relationships (Front-End) kész.

--- app/public/wp-content/themes/unlock-wp-theme/single-program.php
+++ app/public/wp-content/themes/unlock-wp-theme/single-program.php
@@ -2,22 +2,50 @@
 get_header();
 while (have_posts()) {
     the_post(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"> <?php the_title() ?> </h1>
-            <div class="page-banner__intro">
-                <p>Learn how the school of your dreams got started.</p>
-            </div>
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"> <?php the_title() ?> </h1>
+        <div class="page-banner__intro">
+            <p>Learn how the school of your dreams got started.</p>
         </div>
     </div>
-    <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i class="fa fa-home" aria-hidden="true"></i>
-                    All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
-        </div>
-        <div class="generic-content"> <?php echo the_content(); ?> </div>
+</div>
+<div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i
+                    class="fa fa-home" aria-hidden="true"></i>
+                All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
     </div>
+    <div class="generic-content"> <?php echo the_content(); ?> </div>
+    <?php
+            $today = date('Ymd');
+            $homepageEvents = new WP_Query(
+                array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'Event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        ),
+                        array(
+                            'key' => 'related_programs',
+                            'compare' => 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'                        )
+                    )
+                )
+            );
+            while ($homepageEvents->have_posts()) {
+                $homepageEvents->the_post(); ?>
+
+</div>
 <?php }
 get_footer();
 ?>
