0426341c44bf330b3047b3602dfc68bf8b7d28af
unknown <hevesiistvan71@hotmail.com>
2023-09-09 08:47:34 +0200

    045 Reduce Duplication - _get_template_part() KÉSZ

--- app/public/wp-content/themes/unlock-wp-theme/front-page.php
+++ app/public/wp-content/themes/unlock-wp-theme/front-page.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -36,29 +35,12 @@
                 )
             );
             while ($homepageEvents->have_posts()) {
-                $homepageEvents->the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php
-                                                            $eventDate = new DateTime(get_field('event_date'));
-                                                            echo $eventDate->format('M')
-                                                            ?></span>
-                    <span class="event-summary__day"><?php
-                                                            $eventDate = new DateTime(get_field('event_date'));
-                                                            echo $eventDate->format('d')
-                                                            ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a
-                            href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
-
-            <?php }
+                $homepageEvents->the_post();
+                get_template_part('template-parts/content', 'event');
+            }
             ?>
-            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
-                    class="btn btn--blue">View All Events</a></p>
+
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
         </div>
     </div>
@@ -74,23 +56,22 @@
             );
             while ($homepagePosts->have_posts()) {
                 $homepagePosts->the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"><?php the_time('M'); ?></span>
-                    <span class="event-summary__day"><?php the_time('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a
-                            href="<?php the_permalink() ?>"><?php the_title() ?></a>
-                    </h5>
-                    <p> <?php if (has_excerpt()) {
+                <div class="event-summary">
+                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M'); ?></span>
+                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                        </h5>
+                        <p> <?php if (has_excerpt()) {
                                 echo get_the_excerpt();
                             } else {
                                 echo wp_trim_words(get_the_content(), 18);
                             } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
-                    </p>
+                        </p>
+                    </div>
                 </div>
-            </div>
 
             <?php
             }
@@ -107,8 +88,7 @@
 <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -117,8 +97,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -127,8 +106,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Food</h2>
