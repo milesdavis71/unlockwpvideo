7e844c52ed2e989170665e25cc63ca64d21ef8a3
unknown <hevesiistvan71@hotmail.com>
2023-09-16 09:35:15 +0200

    048 Campuses Continued kész

--- app/public/wp-content/themes/unlock-wp-theme/functions.php
+++ app/public/wp-content/themes/unlock-wp-theme/functions.php
@@ -30,6 +30,7 @@ function pagebanner($args = NULL)
 }
 function university_files()
 {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDxfZaI1QRSqkQ2G_Zyli_qqb18_rndyo8', NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style(
     'custom-google-fonts',
@@ -76,3 +77,11 @@ function university_adjust_queries($query)
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api)
+{
+  $api['key'] = 'AIzaSyDxfZaI1QRSqkQ2G_Zyli_qqb18_rndyo8';
+  return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
