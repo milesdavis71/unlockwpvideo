b9abc422c43305e024c8df0f72dfce05f1248620
Istvan <hevesiistvan71@hotmail.com>
2023-09-26 13:23:50 +0200

    053 Managing Time in JavaScript - kész

--- app/public/wp-content/themes/unlock-wp-theme/js/modules/Search.js
+++ app/public/wp-content/themes/unlock-wp-theme/js/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $(".search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
   // 2. events
@@ -16,9 +18,17 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
 
   // 3. methods (function, action...)
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function () {
+      console.log("megy a settimeout");
+    }, 2000);
+  }
+
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOpenOverlay) this.openOverlay();
     if (e.keyCode == 27 && this.isOpenOverlay) this.closeOverlay();
@@ -26,14 +36,12 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
-    console.log("our open method just ran");
     this.isOverlaOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
-    console.log("our close method just ran");
     this.isOverlaOpen = false;
   }
 }
