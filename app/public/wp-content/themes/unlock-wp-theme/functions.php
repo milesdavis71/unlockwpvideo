7e5344bf48ebdcd0b822681f3d36ea6e414f1933
Istvan <hevesiistvan71@hotmail.com>
2023-10-08 17:20:48 +0200

    058 Quick Misc Edits - KÉSZ

--- app/public/wp-content/themes/unlock-wp-theme/functions.php
+++ app/public/wp-content/themes/unlock-wp-theme/functions.php
@@ -30,10 +30,10 @@ function pagebanner($args = NULL)
 }
 function university_files()
 {
-//  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDxfZaI1QRSqkQ2G_Zyli_qqb18_rndyo8', NULL, '1.0', array(
-//      'in_footer' => true,
-//      'strategy'  => 'async',
-//  ));
+  //  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDxfZaI1QRSqkQ2G_Zyli_qqb18_rndyo8', NULL, '1.0', array(
+  //      'in_footer' => true,
+  //      'strategy'  => 'async',
+  //  ));
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style(
     'custom-google-fonts',
@@ -41,6 +41,7 @@ function university_files()
   );
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_localize_script('main-university-js', 'universityData', array('root_url' => get_site_url()));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
@@ -58,9 +59,9 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
-    if (!is_admin() and is_post_type_archive('campus') and $query->is_main_query()) {
-        $query->set('posts_per_page', -1);
-    }
+  if (!is_admin() and is_post_type_archive('campus') and $query->is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
   if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
     $query->set('orderby', 'title');
     $query->set('order', 'ASC');
--- app/public/wp-content/themes/unlock-wp-theme/js/modules/Search.js
+++ app/public/wp-content/themes/unlock-wp-theme/js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 class Search {
   // 1. describe and create/initiate an object
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -38,27 +39,33 @@ class Search {
         this.isSpinnerVisible = false;
       }
 
-      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      this.typingTimer = setTimeout(this.getResults.bind(this), 750);
     }
     this.previousValue = this.searchField.val();
   }
 
   getResults() {
     $.getJSON(
-      "http://localhost:3000/wp-json/wp/v2/posts?search=" +
+      universityData.root_url +
+        "/wp-json/wp/v2/posts?search=" +
         this.searchField.val(),
       (posts) => {
         this.resultsDiv.html(`
         <h2 class="search-overlay__section-title">General Information</h2>
-        <ul class="link-list min-list">
+        ${
+          posts.length
+            ? '<ul class="link-list min-list">'
+            : "<p>No general information matches that search.</p>"
+        }
         ${posts
           .map(
             (item) =>
               `<li><a href="${item.link}">${item.title.rendered}</a></li>`
           )
           .join("")}
-        <ul>
+        ${posts.length ? "<ul>" : ""}
         `);
+        this.isSpinnerVisible = false;
       }
     );
   }
@@ -75,6 +82,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val("");
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlaOpen = true;
   }
 
@@ -82,6 +91,22 @@ class Search {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
     this.isOverlaOpen = false;
+  }
+  addSearchHTML() {
+    $("body").append(`
+    <div class="search-overlay">
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="Mit keresel?" id="search-term">
+        </div>
+    </div>
+    <div class="container">
+        <div id="search-overlay__results"></div>
+    </div>
+</div>
+`);
   }
 }
 
