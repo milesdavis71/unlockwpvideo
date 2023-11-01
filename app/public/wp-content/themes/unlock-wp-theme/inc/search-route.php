c34cb45e58fda29b35747a4d8d6f6abfe4723dd1
Istvan <hevesiistvan71@hotmail.com>
2023-11-01 07:39:02 +0100

    068 Search Logic That's Aware of Relationships kész.

--- app/public/wp-content/themes/unlock-wp-theme/inc/search-route.php
+++ app/public/wp-content/themes/unlock-wp-theme/inc/search-route.php
@@ -66,5 +66,28 @@ function universitySearchResults($data)
             ));
         }
     }
+    $programRelationshipQuery = new WP_Query(array(
+        'post_type' => 'professor',
+        'meta_query' => array(
+            array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => "'81'"
+            )
+
+        )
+    ));
+
+    while ($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+            ));
+        }
+    }
+
     return $results;
 }
