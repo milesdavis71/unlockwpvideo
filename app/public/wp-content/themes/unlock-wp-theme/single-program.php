ba8792cce891d1f038fc5dd483a8da9edf043a6d
Istvan <hevesiistvan71@hotmail.com>
2023-11-05 07:26:45 +0100

    069 Search Logic That's Aware of Relationships (Part 2) kész.

--- app/public/wp-content/themes/unlock-wp-theme/single-program.php
+++ app/public/wp-content/themes/unlock-wp-theme/single-program.php
@@ -4,14 +4,14 @@ while (have_posts()) {
     the_post();
     pagebanner()
     ?>
-    <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i
-                            class="fa fa-home" aria-hidden="true"></i>
-                    All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
-        </div>
-        <div class="generic-content"> <?php echo the_content(); ?> </div>
-        <?php
+<div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i
+                    class="fa fa-home" aria-hidden="true"></i>
+                All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
+    </div>
+    <div class="generic-content"> <?php echo the_field('main_body_content'); ?> </div>
+    <?php
         $relatedProfessors = new WP_Query(
             array(
                 'posts_per_page' => -1,
@@ -33,13 +33,13 @@ while (have_posts()) {
             echo '<ul class="professor-cards">';
             while ($relatedProfessors->have_posts()) {
                 $relatedProfessors->the_post(); ?>
-                <li class="professor-card__list-item">
-                    <a class="professor-card" href="<?php the_permalink() ?>">
-                        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape') ?>">
-                        <span class="professor-card__name"><?php the_title() ?></span>
-                    </a>
-                </li>
-            <?php }
+    <li class="professor-card__list-item">
+        <a class="professor-card" href="<?php the_permalink() ?>">
+            <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape') ?>">
+            <span class="professor-card__name"><?php the_title() ?></span>
+        </a>
+    </li>
+    <?php }
             echo '</ul>';
         }
         wp_reset_postdata();
@@ -81,15 +81,15 @@ while (have_posts()) {
             echo '<h2 class="headline headline--medium">' . get_the_title() . ' availabe at these campuses<h2>';
             echo '<ul class="min-list link-list">';
             foreach ($relatedCampuses as $campus) { ?>
-                <li><a href="<?php the_permalink($campus) ?>"><?php echo get_the_title($campus) ?></a></li>
-                <?php
+    <li><a href="<?php the_permalink($campus) ?>"><?php echo get_the_title($campus) ?></a></li>
+    <?php
 
             }
             echo '</ul>';
         }
         ?>
 
-    </div>
+</div>
 
 <?php }
 
